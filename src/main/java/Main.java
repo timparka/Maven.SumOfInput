fa0541aa5f8298dcc2eaad6fdf867caf99cab253
Timothy <timothy@Zipcoders-MacBook-Pro-3.local>
2023-03-06 08:45:54 -0500

    added changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,11 @@
+import java.util.Scanner;
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args){
-        
+
         int number = getInputNumber();
         
         int sum = sumOfNumbers(number);
@@ -15,12 +16,17 @@ public class Main {
     
     // do this one and the lab is complete. really.
     // return the sum of 0 to n... if n == 3, the result should be 6
-    int sumOfNumbers(int n) {
-        return 0;
+    static int sumOfNumbers(int n) {
+        int num1 = 0;
+        for (int i = 0; i < n; i++) {
+            num1 += i;
+
+        }
+        return num1;
     }
     
     
-    int getInputNumber() {
+    static int getInputNumber() {
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter any number: ");
 
@@ -42,4 +48,6 @@ public class Main {
     // for Extra extra credit - compare the methods and show which one is faster
     // google "how to time some java code"
     // use "System.currentTimeMillis()" to ask the system what time it is.
+    
 }
+
